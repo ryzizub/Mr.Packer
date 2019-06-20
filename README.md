8e491df886e3c56e74467af5ed58b9cbf686e13d
Dominik Šimoník <dominik.simonik@gmail.com>
2019-06-20 22:46:37 +0200

    Update ending of conversation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Mr. Packer
 ![Logo][logo]
 
-[logo]: /icon.png "Mr Packer"
+[logo]: /icon.png "Mr. Packer"
 
 Repository of Action for Google Assistant Mr. Packer.
-Try it now with `Talk to Mr. Paker` in Google Assistant
+Try it now with `Talk to Packing Master` in Google Assistant
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,23 +23,23 @@ app.intent('days', (conv: DialogflowConversation, { duration, number }: { durati
         conv.ask("I don't get for how many days are you going?")
     }
     let days = 0;
-    if (duration.unit === undefined){
+    if (duration.unit === undefined) {
         days = number;
     } else {
-        if (duration.unit === 'day'){
+        if (duration.unit === 'day') {
             days = duration.amount;
         }
-        if (duration.unit === 'year'){
+        if (duration.unit === 'year') {
             days = duration.amount * 365;
         }
-        if (duration.unit === 'month'){
+        if (duration.unit === 'month') {
             days = duration.amount * 30;
         }
-        if (duration.unit === 'wk'){
+        if (duration.unit === 'wk') {
             days = duration.amount * 7;
         }
     }
-    if (days === 0){
+    if (days === 0) {
         conv.ask("I can't help you with trip shorter than 1 day. Probably take some beers and 1 pair of socks.");
         conv.close("Thank you, I can't wait for your next trip");
     } else {
@@ -49,28 +49,38 @@ app.intent('days', (conv: DialogflowConversation, { duration, number }: { durati
     }
 });
 
-app.intent('days - yes',async (conv: DialogflowConversation) => {
+app.intent('days - yes', async (conv: DialogflowConversation) => {
     conv.ask('What are you going to do there?');
     const exp = await expCore.loadExperiences();
     conv.ask(new Suggestions(exp));
 });
 
 app.intent('experience', async (conv: DialogflowConversation, { experience }: { experience: any }) => {
-    if (experience !== null && experience !== undefined){
+    if (experience !== null && experience !== undefined) {
         conv.user.storage.experience = experience;
-        conv.ask("Awesome! Thats nice. Take these things");
-        await expCore.checkAndUpdateExperiences(experience);
+        let response = null;
         if (!conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT')) {
-            conv.ask(await bagCore.countMyList(conv.user.storage.days, experience));
+            response = await bagCore.countMyList(conv.user.storage.days, experience);
         } else {
-            conv.ask(await bagCore.countMyResponse(conv.user.storage.days, experience));
+            response = await bagCore.countMyResponse(conv.user.storage.days, experience);
         }
-        conv.close("Wish you luck on upcoming trip!");
+        conv.ask("Awesome! Thats nice. Take these things.");
+        conv.ask(response);
+        await expCore.checkAndUpdateExperiences(experience);
+        conv.ask("Can I help you again?");
     } else {
         conv.ask('I dont understand you. What are you going to do there?');
         const exp = await expCore.loadExperiences();
         conv.ask(new Suggestions(exp));
     }
+});
+
+
+app.intent('experience - yes',  async (conv: DialogflowConversation) => {
+    conv.user.storage.days = null;
+    conv.user.storage.experience = null;
+    conv.ask('For how many days are you going?');
+    conv.ask(new Suggestions(['1 day', '3 days', '1 week', '2 weeks', '1 month']));
 });
 
 
